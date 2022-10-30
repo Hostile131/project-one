3337c641819e2ec361b37c4b0dcb61cc6ba513d1
Ghazala Ahmed <ghazalaahmed@ghazalas-air.lan>
2022-10-30 14:26:47 -0700

    Added changes to Script

--- Assets/script.js
+++ Assets/script.js
@@ -4,40 +4,56 @@ var streamapikey = 'dcffafa5ebmsh1afad269880e603p159a0fjsn981c2d101bf7'
 
 var reviewsapiUrl =
     'https://www.omdbapi.com/?i=tt3896198&apikey=9e4bbd2d'
-var imgapiURL = 'http://img.omdbapi.com/?apikey=9e4bbd2d'
 var reviewapikey = '9e4bbd2d'
+var searchRatings = document.getElementById('Ratings')
 
 
 function submit() {
+    searchRatings.innerHTML = ''
     console.log('click worked')
     var searchvalue = search.value
     console.log(searchvalue)
+
     var searchUrl = `https://www.omdbapi.com/?t=${searchvalue}&apikey=${reviewapikey}`
     fetch(searchUrl)
     .then(response => response.json())
     .then(response => response)
     .then(response =>{ 
+    var searchTitle = document.getElementById('results')
+    searchTitle.textContent = response.Title
+    var results = response.Ratings
+    for (var i = 0; i < results.length; i++){
+    var ratingList = document.createElement('li')
+    ratingList.textContent = results[i].Source + " : " + results[i].Value
+    searchRatings.appendChild(ratingList)
+    }
+    
     console.log(response)
-    var garray = response.Genre.split(',')
+    console.log(response.Title)
+    console.log(response.Ratings)
 
     fetch(`https://streaming-availability.p.rapidapi.com/v2/search/title?title=${searchvalue}&country=us&type=movie&output_language=en`, options)
 	.then(response => response.json())
-	.then(response => console.log(response))
+	.then(response => {
+        console.log(response)
+        console.log(response.result[0].streamingInfo.us)
+        var Streaming = response.result[0].streamingInfo.us
+        for (var i = 0; i < Streaming.length; i++){
+        var streamingList = document.createElement('li')
+        streamingList.textContent = Streaming[i]
+        }
+    })
 	.catch(err => console.error(err));
 })
 .catch(err => err)
 
-
-    
-
 }
 
-
 var btnSearch = document.getElementById("btnSearch");
-var results = document.getElementById("results");
+// var results = document.getElementById("results");
 var search  = document.getElementById("mediaSearch");
-var reviews = document.getElementById("reviews");
-var genre = ''
+// var reviews = document.getElementById("reviews");
+// var genre = ''
 
 const options = {
     method: 'GET',
@@ -46,29 +62,5 @@ const options = {
         'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
     }
 };
-fetch('https://streaming-availability.p.rapidapi.com/search/pro?country=us&service=netflix&type=movie&order_by=original_title&year_min=2000&year_max=2020&genre=18&page=1&desc=true&language=en&output_language=en', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
-// fetch (apiUrl)
-// .then(function(resp){
-//     if (resp.ok){
-//         return resp.json();
-//     }
-// })
-// .then(function(data) {
-//     if (!data) {
-//         return
-//     }
-//     var{ results} = data
-//     for(var i= 0; i < results.length; i++){
 
-//     }
-// })
-
-fetch('https://www.omdbapi.com/?i=tt3896198&apikey=9e4bbd2d')
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
-    
-    btnSearch.addEventListener('click', submit)
+ btnSearch.addEventListener('click', submit)
